ed64fb3c487c1c1243a229823db7ca1734454b07
Dan Toms <157254317+pynmash@users.noreply.github.com>
2024-05-26 21:55:09 +0100

    Position widgets and make chat_area read only

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ class ChatbotWindow(QMainWindow):
 
         # Add chat area widget
         self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480,  320)
+        self.chat_area. setReadOnly(True)
         
 
 
         # Add the input field widget
         self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10, 340, 480, 40)
 
 
         # Add the send button
         self.button = QPushButton("Send", self)
+        self.button.setGeometry(500, 340, 100, 40)
 
         self.show()
 
