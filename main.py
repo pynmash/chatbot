eb361ee9f104541a92ca13ff07cf4b606711cff7
Dan Toms <157254317+pynmash@users.noreply.github.com>
2024-05-27 01:03:12 +0100

    Add threading to improve UX, enable send on enter, frontend connected to backend

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PyQt6.QtWidgets import QApplication, QLineEdit, QMainWindow, QPushButton,\
     QTextEdit
 
 import sys
+from backend import Chatbot
+import threading
 
 
 
@@ -9,6 +11,8 @@ import sys
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(610, 390)
 
@@ -23,15 +27,31 @@ class ChatbotWindow(QMainWindow):
         # Add the input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_message)
 
 
         # Add the send button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
 
 
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#666666'><strong>ME:</strong> {user_input}</p>")
+        self.input_field.clear()
+
+        thread = threading.Thread(
+            target=self.get_bot_response, 
+            args=(user_input, )
+        )
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color:#E9E9E9'><strong>Chatbot:</strong> {response}</p>")
 
 
 
